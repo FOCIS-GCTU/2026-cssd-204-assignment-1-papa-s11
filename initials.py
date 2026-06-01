e8ec1a35282c13690112cafb1a5a32535259d820
papa-s11 <sakyiansah64@gmail.com>
2026-06-01 11:08:34 +0000

    Print initials with capital letters

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Kwabena Sakyi Ansah
+# STUDENT ID:  2425403835
+# Email: 2425403835@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Kwabena Sakyi Ansah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
-
-
-
+    print()
+    print("...KSA")
+    print()
+    print("...KK.......KK.........SSSSSSSS..............AAAA...........")
+    print("...KK.......KK........SS........SS..........AA.AA...........")
+    print("...KK......KK.........SS...................AA..AA...........")
+    print("...KK...KKK...........SS..................AA...AA...........")
+    print("...KKKKKK..............SSSSSSS...........AA.....AA..........")
+    print("...KKKKKK.....................SSSS.......AAAAAAAAA..........")
+    print("...KK...KKK.....................SS.......AA.....AA..........")
+    print("...KK......KK...................SS.......AA.....AA..........")
+    print("...KK.......KK..**....SS.......SS..**....AA.....AA....**....")
+    print("...KK.......KK..**.....SSSSSSSS....**....AA.....AA....**....")
+    print()
 main()
+
